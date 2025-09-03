215894a32ff0b257fd53fb789a96eb75e077db67
Cyra <claireaus066@gmail.com>
2025-09-03 14:18:57 +1000

    Github FastAPI CI/CD pipeline to AWS

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 from .routers import chat
 
 app = FastAPI(title="MindPal API", version="1.0.0")
@@ -25,3 +26,5 @@ def health():
 
 
 app.include_router(chat.router)
+
+handler = Mangum(app)
