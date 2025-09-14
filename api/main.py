de887df7db91f7b4cb63ec759d2e7295b74f8da8
myou0038 <myou0038@student.monash.edu>
2025-09-14 15:25:43 +1000

    Added more database API allowed endpoints to main.py

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
     allow_credentials=False,
-    allow_methods=["POST", "OPTIONS"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Content-Type", "Accept", "Authorization", "x-session-id"],
 )
 
